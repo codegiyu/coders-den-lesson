7548c150a16205771bcae524f726dca49b567346
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-02-11 16:43:30 +0100

    created the repo

---
+++ introduction.js
@@ -0,0 +1,38 @@
+console.log(2 + 2);
+let error = "something went wrong"
+console.log(2, "second thing", false)
+console.log('Error:', error)
+
+// PRIMITIVE DATA TYPES
+// String - "fcgvlhyy", '245' `false`, 
+// Number - 112, 1.2, 0, 
+// Boolean - true false, 
+// undefined, 
+// Null, 
+// Symbol
+
+// NON-PRIMITIVE DATA TYPES
+// Object - {}
+// Array - []
+
+let random = undefined;
+// const random = undefined;
+// var random = undefined;
+random = null
+console.log(typeof random)
+
+// let const
+
+let myString = 11
+
+const thisIsMyName = "Giyu"
+const myDNS = myString = 45
+console.log(myDNS)
+
+const firstName = 'Asabeneh', lastName = 'Yetayeh'
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age = 100 // age in years
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age, isMarried)
