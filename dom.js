b15dde4401e0b41c50a63d2809dcecb1054ebef8
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-08-26 17:34:13 +0100

    create, append, delete elements

--- dom.js
+++ dom.js
@@ -5,6 +5,13 @@ const thirdTitle = document.querySelector(".title");
 const allTitles = document.querySelectorAll(".title");
 const nick = document.querySelector("h1[data-nickname=hello]")
 const child = document.getElementById("boy");
+const gridPapa = document.querySelector(".grid-container");
+const gridMama = document.getElementsByClassName("grid-container")[1];
+const [indexInput, colorInput] = document.getElementsByClassName("input");
+const fileInput = document.getElementById("file");
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.querySelector("#btn-2");
+const imgBox = document.querySelector(".selected-image").firstElementChild;
 
 console.log("h1Tags: ", h1Tags);
 console.log("titleClass: ", titleClass);
@@ -58,6 +65,9 @@ console.log(child.innerHTML);
 // child.textContent = "I'm a man now. I still live <b>in Abuja as well</b>.<div style='width: 100px; height: 100px; background: blue;'></div> We fit no see sha.";
 child.innerHTML = "I'm a man now. I still live <b>in Abuja as well</b>.<div style='width: 100px; height: 100px; background: blue;'></div> We fit no see sha.";
 
+console.log("Last Child: ", child.lastChild);
+console.log("Last Element Child: ", child.lastElementChild);
+
 console.log(child.style);
 child.style.fontFamily = "cursive";
 child.style.textAlign = "right";
@@ -66,3 +76,68 @@ console.log(child.style.fontSize);
 
 const { width, height, fontFamily, fontSize, display, color, border } = window.getComputedStyle(child);
 console.log(width, height, fontFamily, fontSize, display, color, border);
+
+let box;
+let text;
+let content;
+for (let i = 0; i < 10; i++) {
+    box = document.createElement("div");
+    box.className = "box yellow"
+    box.setAttribute("id", `box-${i}`)
+    box.style.width = "100%"
+    box.style.height = "150px";
+
+    text = document.createElement("p");
+    text.textContent = i;
+
+    box.appendChild(text)
+
+    gridPapa.appendChild(box);
+
+    content =   `<div 
+                    class="box blue" 
+                    id="box-${i}"
+                    style="width: 100%, height: 150px"
+                >
+                    <p>${i}</p>
+                </div>`
+    gridMama.innerHTML += content;
+}
+
+fileInput.addEventListener('change', selectImage);
+btn1.addEventListener('click', changeBoxColor);
+btn2.addEventListener('click', deleteLastBox);
+
+function deleteLastBox() {
+    const lastChild = gridPapa.lastElementChild;
+    const lastChild2 = gridMama.lastElementChild;
+
+    gridPapa.removeChild(lastChild);
+    gridMama.removeChild(lastChild2);
+}
+
+function changeBoxColor() {
+    const index = Number(indexInput.value);
+    const color = colorInput.value;
+    const target = gridPapa.children[index];
+
+    console.log("Target: ", target);
+
+    if (target) {
+        target.style.background = color;
+        console.log("target exists");
+    }
+
+    indexInput.value = "";
+    colorInput.value = "";
+}
+
+function selectImage(event) {
+    const image = event.target.files[0];
+    console.log(image);
+
+    const imgVal = URL.createObjectURL(image);
+    console.log(imgVal);
+
+    imgBox.src = imgVal
+}
